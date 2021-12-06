ce6b178e503a7e017959e5e2a618d1a9a050c808
Clair Luis Jevinski <cl.jevinski@hotmail.com>
2021-12-06 08:12:26 -0300

    Special move promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +23,7 @@ public class ChessMatch {
 	private Board board;
 	private Colour currentPlayer;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
 	private List<Piece> piecesOnBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
 
@@ -73,6 +75,15 @@ public class ChessMatch {
 		
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
 		
+		// special move promotion
+		promoted = null;
+		if(movedPiece instanceof Pawn) {
+			if(movedPiece.getColour() == Colour.WHITE && target.getRow() == 0 || movedPiece.getColour() == Colour.BLACK && target.getRow() == 7) {
+				promoted = (ChessPiece) board.piece(target);
+				promoted = replacePromotedPiece("Q");
+			}
+		}
+		
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		
 		if(testCheckMate(opponent(currentPlayer))) {
@@ -90,6 +101,35 @@ public class ChessMatch {
 		
 		return (ChessPiece) capturedPiece;
 		
+	}
+	
+	public ChessPiece replacePromotedPiece(String type) {
+		if(promoted == null) {
+			throw new IllegalStateException("There is no piece to be promoted");
+		}
+		
+		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+			throw new InvalidParameterException("Invalid type for promotion");
+		}
+		
+		Position pos = promoted.getChessPosition().toPosition();
+		Piece piece = board.removePiece(pos);
+		
+		piecesOnBoard.remove(piece);
+		
+		ChessPiece newPiece = newPiece(type, promoted.getColour());
+		
+		board.placePiece(newPiece, pos);
+		piecesOnBoard.add(newPiece);
+		
+		return newPiece;
+	}
+	
+	private ChessPiece newPiece(String type, Colour colour) {
+		if(type.equals("B")) return new Bishop(board, colour);
+		if(type.equals("N")) return new Knight(board, colour);
+		if(type.equals("Q")) return new Queen(board, colour);
+		return new Rook(board, colour);
 	}
 	
 	private void validateSourcePosition(Position position) {
@@ -329,6 +369,10 @@ public class ChessMatch {
 	
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
+	}
+	
+	public ChessPiece getPromoted() {
+		return promoted;
 	}
 
 }
